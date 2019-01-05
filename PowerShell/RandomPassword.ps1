e7588a6b8391f9f58ff3e32be53c5297b303f38c
Myers Chan <Nomyfan@live.com>
2019-01-05 10:17:16 +0800

    Update RandomPassword.ps1 Now it has capability to generate random password with special characters.

--- PowerShell/RandomPassword.ps1
+++ PowerShell/RandomPassword.ps1
@@ -3,21 +3,60 @@ PSVersion: 5.1
 ---------------------------------------------------------------------
 This script is used to generate a random password with a given length.
 ---------------------------------------------------------------------
+Example1: .\RandomPassword.ps1
+Example2: .\RandomPassword.ps1 -Length 12
+Example3: .\RandomPassword.ps1 -Length 12 -PunctuationType Custom -Punctuation '_','*','^','@'
 #>
 
 param (
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [uint16]
-    $Length
+    $Length,
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('All', 'Custom')]
+    [string]
+    $PunctuationType,
+    [Parameter(Mandatory = $false)]
+    [string[]]
+    $Punctuation
 )
 
-$Length = if ($Length -lt 10) {10} else {$Length}
+$Length = if ($Length -lt 8) {8} else {$Length}
 $rnd = New-Object System.Random
 $password = @()
 
-foreach ($i in 1..$Length) {
-    $password += [char]($rnd.Next(33, 127))
+function Write-Password {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string[]]
+        $Pwd
+    )
+    Write-Host -NoNewline 'Output: '
+    Write-Host ($Pwd -join '') -BackgroundColor Black -ForegroundColor Green
 }
 
-Write-Host -NoNewline 'Output: '
-Write-Host ($password -join '') -BackgroundColor Black -ForegroundColor Green
+if ($PunctuationType.Length -eq 0 -or $PunctuationType.ToLower().Equals('all')) {
+    1..$Length | ForEach-Object {
+        $password += [char]($rnd.Next(33, 127))
+    }  
+    Write-Password -Pwd $password
+}
+else {
+    # Upper case
+    $rndMap = [char]'A'..[char]'Z'
+    # Lower case
+    $rndMap += [char]'a'..[char]'z'
+    # Numeric
+    $rndMap += [char]'0'..[char]'9'
+    # Punctuation
+    $Punctuation | ForEach-Object {
+        if (([char]$_) -in 33..126 -and ([char]$_) -notin $rndMap) {
+            $rndMap += $_
+        }
+    }
+
+    1..$Length | ForEach-Object {
+        $password += [char]($rndMap[$rnd.Next(0, $rndMap.Length)])
+    }
+    Write-Password -Pwd $password
+}
