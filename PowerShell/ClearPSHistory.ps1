17a856010bfe864dbead89710118435f1bcd242b
Myers Chan <Nomyfan@live.com>
2019-01-05 10:20:31 +0800

    Add ClearPSHistory.ps1 Clear PS execution history.

---
+++ PowerShell/ClearPSHistory.ps1
@@ -0,0 +1,7 @@
+<#
+PSVersion: 5.1
+-----------------------------------------------------------------
+Clear your execution history in Powershell.
+-----------------------------------------------------------------
+#>
+Clear-Content "$HOME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
