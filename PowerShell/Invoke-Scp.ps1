e47f85f2bfdd64a13c624ebae80fb2848a8b9bf4
nomyfan <Nomyfan@live.com>
2019-12-24 15:48:46 +0800

    Read servers from JSON file

--- PowerShell/Invoke-Scp.ps1
+++ PowerShell/Invoke-Scp.ps1
@@ -15,36 +15,30 @@ Example: Invoke-Scp -LocalPath ./config.json -Operation Push
         [Parameter(Mandatory = $true)]
         [ValidateSet("Fetch","Push")]
         [Alias("o")]
-        [string]$Operation
+        [string]$Operation,
+        [Parameter(Mandatory = $false)]
+        [string]$ServersJsonPath = "./servers.json"
     )
-    $servers = @{
-        server1 = @{
-            port = 2333
-            user = "username"
-            ip = "127.0.0.1"
+    $servers = @{}
+    (Get-Content -Path $ServersJsonPath | ConvertFrom-Json).servers | ForEach-Object { $servers[$_.name] = $_ }
+
+    if ($servers.Keys.Count -ne 0) {
+        $serverName = Read-Host -Prompt (-Join "select target server (", $servers.Keys, ")")
+        $defaultPath = -Join ("/home/",$servers.$serverName.user )
+        $serverPath = Read-Host -Prompt (-Join ("input server path(default:",$defaultPath,")"))
+    
+        if ($serverPath -eq "") {
+            $serverPath = $defaultPath
         }
-        server2 = @{
-            port = 5555
-            user = "username"
-            ip = "127.0.0.1"
+    
+        $server =  (-Join ($servers.$serverName.user,"@",$servers.$serverName.ip))
+        $serverPath = (-Join ($server, ":", $serverPath))
+    
+        if ($Operation -eq "Push") {
+            scp -P $servers.$serverName.port $LocalPath $serverPath
         }
-    }
-
-    $serverName = Read-Host -Prompt (-Join "select target server (", $servers.Keys, ")")
-    $defaultPath = -Join ("/home/",$servers[$serverName]["user"] )
-    $serverPath = Read-Host -Prompt (-Join ("input server path(default:",$defaultPath,")"))
-
-    if ($serverPath -eq "") {
-        $serverPath = $defaultPath
-    }
-
-    $server =  (-Join ($servers[$serverName]["user"],"@",$servers[$serverName]["ip"]))
-    $serverPath = (-Join ($server, ":", $serverPath))
-
-    if ($Operation -eq "Push") {
-        scp -P $servers[$serverName]["port"] $LocalPath $serverPath
-    }
-    else {
-        scp -P $servers[$serverName]["port"] $serverPath $LocalPath
+        else {
+            scp -P $servers.$serverName.port $serverPath $LocalPath
+        }
     }
 # }
