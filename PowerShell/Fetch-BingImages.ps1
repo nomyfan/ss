c897e007a47c564a7b3c37d25e2fcbbdff821eb6
nomyfan <Nomyfan@live.com>
2021-06-13 01:35:02 +0800

    Fetch-BingImages

---
+++ PowerShell/Fetch-BingImages.ps1
@@ -0,0 +1,59 @@
+<#
+PSVersion: 7.1.3
+--------------------------------
+Download latest images from Bing
+--------------------------------
+Usage:
+- Use -Index to specify staring index, default is 0
+- Use -Count to specify how many images from -Index to download, default is 8(it seems to be the maximum number)
+- Use -SaveTo to specify where the images save to, default is current directory
+#>
+
+[CmdletBinding()]
+param (
+  [Parameter()]
+  [uint]
+  $Index,
+  [Parameter()]
+  [uint]
+  $Count,
+  [Parameter()]
+  [string]
+  $SaveTo
+)
+
+if($Count -eq 0) {
+  $Count = 8
+}
+
+if([System.String]::IsNullOrEmpty($SaveTo)) {
+  $SaveTo = './'
+}
+
+if(!(Test-Path -Path $SaveTo)) {
+  Write-Error ("{0} doesn't exit" -f $SaveTo)
+  return
+}
+
+function ExtractInfo {
+  param (
+    [Parameter(Mandatory)]
+    [string]
+    $Url
+  )
+
+  $uri = [System.Uri]('https://cn.bing.com{0}' -f $Url)
+  $parsedQuery = [System.Web.HttpUtility]::ParseQueryString($uri.Query)
+
+  return [PSCustomObject]@{
+    FileName = $parsedQuery['id']
+    Uri = $uri
+  }
+}
+
+$uri = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx={0}&n={1}&mkt=zh-CN' -f $Index,$Count
+$response = Invoke-WebRequest -Uri $uri
+
+($response.Content | ConvertFrom-Json).images | ForEach-Object {(ExtractInfo -Url $_.url)} | ForEach-Object -Parallel {
+  Invoke-WebRequest -Uri $_.Uri -OutFile ("{0}{1}" -f $SaveTo,$_.FileName)
+}
