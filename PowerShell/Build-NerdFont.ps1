843fdb6d733bf16377c495455d50a0e178294578
nomyfan <Nomyfan@live.com>
2023-07-02 10:58:21 +0800

    makegroups 1

--- PowerShell/Build-NerdFont.ps1
+++ PowerShell/Build-NerdFont.ps1
@@ -30,5 +30,5 @@ function Get-NumberOfLogicalProcessors {
 Get-ChildItem $In -File | ForEach-Object -Parallel {
   $fullName = $_.FullName
   $name = $_.Name
-  Start-Process -PassThru -Wait -FilePath 'docker' -ArgumentList 'run', '--rm', '-v', """${fullName}"":""/in/${name}""", '-v', """${using:Out}"":/out", 'nerdfonts/patcher:4.4.0', '--complete', '--careful', '--makegroups', '0'
+  Start-Process -PassThru -Wait -FilePath 'docker' -ArgumentList 'run', '--rm', '-v', """${fullName}"":""/in/${name}""", '-v', """${using:Out}"":/out", 'nerdfonts/patcher:4.4.0', '--complete', '--careful', '--makegroups', '1'
 } -ThrottleLimit (Get-NumberOfLogicalProcessors)
