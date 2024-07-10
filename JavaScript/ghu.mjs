d738e07de85f024a34ad199af40d8167a9eed6f1
nomyfan <Nomyfan@live.com>
2024-07-10 22:31:49 +0800

    Support SSH over the HTTPS port

--- JavaScript/ghu.mjs
+++ JavaScript/ghu.mjs
@@ -1,41 +1,53 @@
 import process from "node:process";
 import { execSync } from "node:child_process";
 
-const argvs = process.argv.slice(2);
+const argv = process.argv.slice(2);
 
 const isOption = (s) => s.startsWith("--");
-const options = argvs.filter((arg) => isOption(arg));
-const args = argvs.filter((arg) => !isOption(arg));
+const options = argv.filter((arg) => isOption(arg));
+const args = argv.filter((arg) => !isOption(arg));
 
-const owner = args[0];
-const repo = args[1];
+function parseOwnerRepo(args) {
+  const first = args[0];
 
-const usage = `Usage: ghu [OPTIONS] <owner> <repo>
+  let match = /([^\/:]+)[/]([^\/]+)\.git$/.exec(first);
+  if (match) {
+    return [match[1], match[2]];
+  } else if ((match = /([^\/]+)[/]([^\/]+)$/.exec(first))) {
+    return [match[1], match[2]];
+  } else {
+    return [args[0], args[1]];
+  }
+}
+
+const usage = `Usage: ghu [OPTIONS] <owner> <repo> / ghu [OPTIONS] <URL>
 OPTIONS:
 --https
-  Clone URL in HTTPS protocol.
---git
-  Clone URL in git protocol. This is default if you don't specify --https.
+  Using HTTPS protocol.
+--ssh
+  Using SSH protocol. Default.
+--sh
+  Using SSH protocol over HTTPS port.
 --clone
   Execute \`git clone\`
 `;
 
-if (!owner) {
-  console.error(usage);
-  process.exit(1);
-}
+const [owner, repo] = parseOwnerRepo(args);
 
-if (!repo) {
+if (!owner || !repo) {
   console.error(usage);
   process.exit(1);
 }
 
 const https = options.includes("--https");
+const sshOverHttps = options.includes("--sh");
 // Exec `git clone`
 const clone = options.includes("--clone");
 
 const url = https
   ? `https://github.com/${owner}/${repo}.git`
+  : sshOverHttps
+  ? `ssh://git@ssh.github.com/${owner}/${repo}.git`
   : `git@github.com:${owner}/${repo}.git`;
 
 if (clone) {
