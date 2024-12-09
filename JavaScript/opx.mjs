bf24b682db7fa597ab5c492b4288ebc332e3d32c
nomyfan <Nomyfan@live.com>
2024-12-09 20:00:22 +0800

    Update opx

--- JavaScript/opx.mjs
+++ JavaScript/opx.mjs
@@ -7,42 +7,38 @@ const argv = process.argv.slice(2);
 
 const isOption = (s) => /^-(([a-zA-Z])|(-[a-zA-Z]\w+))(=.*)?$/.test(s);
 
-/**
- * @param {string} cmd
- * @param {boolean} trim
- * @returns
- */
-function sh(cmd, trim = true) {
-  const output = execSync(cmd, { encoding: "utf-8" });
-  if (trim) {
-    return output.trim();
-  }
-  return output;
+function run(command, trim = true) {
+  const output = execSync(command, { encoding: "utf-8" });
+  return trim ? output.trim() : output;
 }
 
-function die(message, code = 1) {
+function exit(message, code = 1) {
   process.stderr("error: %s\n", message);
   process.exit(code);
 }
 
 function opengh() {
-  const options = argv.slice(1).filter((arg) => isOption(arg));
+  const args = argv.slice(1).filter((arg) => !isOption(arg));
 
   try {
-    const output = sh("git remote get-url origin");
+    const output = run("git remote get-url origin");
     const RE =
       /^(?:git@github\.com:|https:\/\/github\.com\/|ssh:\/\/git@(?:ssh\.)?github.com(?::443)?\/)([^\/]+)\/([^\/]+)\.git$/;
 
     const match = RE.exec(output);
     if (match) {
       const [, owner, repo] = match;
-      const rev = sh("git rev-parse --abbrev-ref HEAD");
-
-      let url = `https://github.com/${owner}/${repo}/tree/${rev}`;
-      if (options.includes("-p")) {
-        const p = sh("git rev-parse --show-prefix").slice(0, -1);
-        url += `/${p}`;
+      const rev = run("git rev-parse --abbrev-ref HEAD");
+      let subpath = undefined;
+      if (args.length > 0) {
+        const gitRoot = run("git rev-parse --show-toplevel");
+        const absPath = path.resolve(process.cwd(), args[0]);
+        if (absPath.startsWith(gitRoot)) {
+          subpath = path.relative(gitRoot, absPath);
+        }
       }
+
+      const url = `https://github.com/${owner}/${repo}/tree/${rev}${subpath ? `/${subpath}` : ""}`;
       execSync(`open ${url}`);
     }
   } catch (err) {
@@ -67,7 +63,7 @@ async function opennpm() {
     ),
   );
   if (!packageJson.name) {
-    die("No package name found in package.json");
+    exit("No package name found in package.json");
   }
   execSync(`open https://www.npmjs.com/package/${packageJson.name}`);
 }
@@ -76,7 +72,7 @@ async function main() {
   const subcommand = argv[0];
   const subcommands = ["gh", "npm"];
   if (!subcommands.includes(subcommand)) {
-    die("Available subcommands: gh, npm");
+    exit("Available subcommands: gh, npm");
   }
 
   switch (subcommand) {
